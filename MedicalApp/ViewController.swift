5df8652a135c1b35f38d330d9a5471ccea5327fa
Marwa Kharbech <marwa.kharbeche@gmail.com>
2017-02-20 17:57:46 -0500

    Design first UI

--- MedicalApp/ViewController.swift
+++ MedicalApp/ViewController.swift
@@ -8,11 +8,21 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
 
+    @IBOutlet weak var picker: UIPickerView!
+   
+    
+    
+    @IBOutlet weak var pickerButton: UIButton!
+    
+    let States = [ "Alberta","British Columbia","Manitoba","New Brunswick","Newfoundland and Labrador","Northwest Territories","Nova Scotia","Nunavut","Ontario","Prince Edward Island","Quebec","Saskatchewan","Yukon"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        picker.delegate = self
+        picker.dataSource = self
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +30,26 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return States.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return States[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        pickerButton.setTitle (States[row], for: UIControlState.normal)
+        picker.isHidden = true
+    }
+    
+    @IBAction func pickerButtonTapped(_ sender: Any) {
+         picker.isHidden = false
+    }
 
 }
 
